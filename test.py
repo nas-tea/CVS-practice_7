340526438530b5785d8a2c036c6c34214962bf52
nas-tea <hkst24@mail.ru>
2024-02-08 10:40:27 +0500

    commit dev

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+#1
+print("Задание 1")
 s=0
 
 while True:
@@ -18,3 +20,32 @@ for i in range(int(k), int(m) + 1):
     if i < int(m) + 1:
         s += i ** 2
 print(s)
+
+#2
+print("Задание 2")
+import math
+
+while True:
+    n = input("Введите n: ")
+    if n.isdigit()==False:
+        print("Введите ещё раз ")
+    else:
+        break
+
+max=0
+for k in range(1, int(n)+1):
+    if math.factorial(k) <= int(n):
+        max = k
+    else:
+        break
+
+k=max
+if n==0:
+    k=n
+print(k)
+
+#3
+print("Задание 3")
+
+for i in range(2, 11):
+    print(' '.join(str(i*v) for v in range(2, 11)))
